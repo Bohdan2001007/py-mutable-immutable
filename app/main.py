4db1c02c4930d5be0e4e039da130d4c828fac2e0
Bohdan Chernysh <bogdan1991007@gmail.com>
2022-08-19 00:27:27 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,19 +1,13 @@
-lucky_number = 777
-pi = 3.14
-one_is_a_prime_number = False
-name = "Richard"
-my_favourite_films = [
-    "The Shawshank Redemption",
-    "The Lord of the Rings: The Return of the King",
-    "Pulp Fiction",
-    "The Good, the Bad and the Ugly",
-    "The Matrix",
-]
-profile_info = ("michel", "michel@gmail.com", "12345678")
-marks = {
-    "John": 4,
-    "Sergio": 3,
-}
-collection_of_coins = {1, 2, 25}
+a = True
+b = 3
+c = "ееп"
+d = [3, 5]
+e = {1: "tgb", 2: "рвц"}
+f = 3.5
+g = {"apple", "banana", "cherry"}
+h = ("apple", "banana", "cherry")
 
-# write your code here
+sorted_variables = {
+    "mutable": [d, e, g],
+    "immutable": [a, b, c, f, h]
+}
